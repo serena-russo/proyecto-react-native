07868b5455520c3f2e22aee5f0e4ccdac0576f3e
serena-russo <srusso@udesa.edu.ar>
2023-11-06 16:00:51 -0300

    lo basico de register, home, login y app

--- my-app/App.js
+++ my-app/App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { auth } from "./src/firebase/config";
+import Register from './src/screens/Register/Register';
+import Login from './src/screens/Login/Login';
+import Home from './src/screens/Home/Home';
+
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Login' component={Login} options={{headerShown: false}}/>
+        <Stack.Screen name='Register' component={Register} options={{headerShown: false}}/>
+        <Stack.Screen name='Home' component={Home} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -15,6 +28,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
