6c77c6d67b461446fcbb7bf4ff7af8c22390b7ae
serena-russo <srusso@udesa.edu.ar>
2023-11-13 12:24:13 -0300

    form y postForm

---
+++ my-app/src/components/Posts/Posts.js
@@ -0,0 +1,113 @@
+import React, {Component} from 'react';
+import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
+import {db, auth} from '../../firebase/config';
+import firebase from 'firebase';
+
+
+class Post extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            like: false,
+            cantidadDeLikes: this.props.dataPost.datos.likes.length
+        }
+    }
+
+    componentDidMount(){
+        //Chequiar apenas carga si el post esta o no likeado
+        if (this.props.dataPost.datos.likes.includes(auth.currentUser.email)){
+            this.setState({
+                like:true
+            })
+        }
+    }
+
+    //Necesitamos en FB que cada Post tenga una propiedad con un array de emails
+
+    likear(){
+        //Agrega un email en la propiedad like del post
+        db.collection('posts').doc(this.props.dataPost.id).update({
+            likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)         
+        })
+        .then( res => this.setState({
+            like:true,
+            cantidadDeLikes: this.props.dataPost.datos.likes.length
+        })
+        )
+        .catche(e => console.log(e))
+    }
+
+    unlike(){
+        //Quita un email de la propiedad like del post
+        db.collection('posts').doc(this.props.dataPost.id).update({
+            likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)         
+        })
+        .then( res => this.setState({
+            like:false,
+            cantidadDeLikes: this.props.dataPost.datos.likes.length
+        })
+        )
+        .catche(e => console.log(e))
+    }
+
+
+
+    render (){
+        console.log(this.props)
+        return(
+            <View>
+                <Text> {this.props.dataPost.datos.owner} </Text>
+                <Text> {this.props.dataPost.datos.textoPost} </Text>
+                <Text> Cantidad de likes: {this.state.cantidadDeLikes} </Text>
+
+                {
+                    this.state.like ?
+                    <TouchableOpacity style={styles.button}onPress={()=>this.unlike()}>
+                        <Text style={styles.textButton}>Unlike</Text>    
+                    </TouchableOpacity>
+
+                    :
+
+                    <TouchableOpacity style={styles.button} onPress={()=>this.likear()}>
+                        <Text style={styles.textButton}>Likear</Text>    
+                    </TouchableOpacity>
+                }
+
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    formContainer:{
+        paddingHorizontal:10,
+        marginTop: 20,
+    },
+    input:{
+        height:20,
+        paddingVertical:15,
+        paddingHorizontal: 10,
+        borderWidth:1,
+        borderColor: '#ccc',
+        borderStyle: 'solid',
+        borderRadius: 6,
+        marginVertical:10,
+    },
+    button:{
+        backgroundColor:'pink',
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        textAlign: 'center',
+        borderRadius:4, 
+        borderWidth:1,
+        borderStyle: 'solid',
+        borderColor: 'pink'
+    },
+    textButton:{
+        color: '#fff'
+    }
+
+})
+
+export default Post
