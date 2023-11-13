9bafb8126a43f0bbaca84686585b46023717f973
serena-russo <srusso@udesa.edu.ar>
2023-11-13 12:38:46 -0300

    modificacion

--- my-app/App.js
+++ my-app/App.js
@@ -4,6 +4,7 @@ import { auth } from "./src/firebase/config";
 import Register from './src/screens/Register/Register';
 import Login from './src/screens/Login/Login';
 import Home from './src/screens/Home/Home';
+import PostForm from './src/screens/PostForm/PostForm'
 
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
