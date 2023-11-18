1e5763c7f3d2e15671af022f99fe34c747b8f672
serena-russo <srusso@udesa.edu.ar>
2023-11-18 11:07:30 -0300

    mi perfil -> avance pero no esta terminado

--- my-app/src/screens/Perfil/Perfil.js
+++ my-app/src/screens/Perfil/Perfil.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, FlatList, StyleSheet, Image } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, StyleSheet, Image, ScrollView } from 'react-native';
 import { auth, db } from '../../firebase/config';
 
 class Perfil extends Component {
@@ -52,13 +52,69 @@ class Perfil extends Component {
     render(){
         console.log(this.state);
         return(
-           <View>
-                <TouchableOpacity onPress={() => this.logout()}>
-                    <Text> Cerrar sesión</Text>
+            <ScrollView style= {styles.contenedor}>
+
+                <View  style={styles.info3}>
+                    <Text style= {styles.usuario}>Bienvenidi {this.state.infoUser.userName}</Text>
+                    <Text style= {styles.bio}>Biografia: {this.state.infoUser.miniBio}</Text>
+                    <Text style= {styles.mail}>Mail: {this.state.infoUser.userName}</Text>
+                    <Image style= {styles.imagenP} source={{uri: this.state.infoUser.imagenP}}/>
+                </View>
+
+                <Text style={styles.titulos}>Mis posteos:</Text>
+                <FlatList>
+                    data={this.state.props}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item})=> <Post dataPost={item}/>}
+                </FlatList>
+           
+                <TouchableOpacity onPress={() => this.logout()} style={styles.botonSalir}>
+                    <Text style={styles.mail}> Cerrar sesión</Text>
                 </TouchableOpacity>
-            </View>
+            
+            </ScrollView>
         )
     }
-
 }
+
+const styles = StyleSheet.create({
+    contenedor: {
+        backgroundColor: 'green',
+        padding: 18,
+    },
+    info3: {
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    usuario: {
+        fontSize: 20,
+        marginBottom: 8,
+    },
+    bio: {
+        fontSize: 20,
+        marginBottom: 8,
+    },
+    mail: {
+        fontSize: 16,
+        marginBottom: 15,
+    },
+    imagenP: {
+        width: 150,
+        height: 150,
+        borderRadius: 80,
+    },
+    titulos: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    botonSalir: {
+        backgroundColor: 'red',
+        padding: 10,
+        borderRadius: 10,
+        alignItems: 'center',
+        marginTop: 15,
+    },
+})
+
 export default Perfil
